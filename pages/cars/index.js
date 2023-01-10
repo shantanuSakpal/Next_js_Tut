9a2ffbf9cfcd6e7155a8bb1526f5393c0d3859a0
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-01-10 11:59:22 +0530

    initial commit

---
+++ pages/cars/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function CarList() {
+    return (
+        <div>
+            <ul>
+                This is the cars list:
+                <li><a href="http://localhost:3000/cars/ford">Ford</a></li>
+                <li><a href="http://localhost:3000/cars/Lambo">lambo</a></li>
+                <li><a href="http://localhost:3000/cars/tesla">tesla</a></li>
+            </ul>
+        </div>
+    )
+}
